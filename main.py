d5a51a442b902a0a585e9303ddb4ce4c30fcb68a
Karan Patel <karan.patel@healthsparq.com>
2019-07-21 16:47:37 -0700

    Adding import from

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # imports must find files, compile them to byte code, and run the code.
-import sys  # Load a library module
+import sys                  # Load a library module
 
-import module  # import a module, each module file is a package of variables—that is, a namespace
+import module               # import a module, each module file is a package of variables—that is, a namespace
+from module import var1     # from copies a module’s attributes, such that they become simple variables in the recipient
 
 var1 = "This will overridden var1 reference by the by the 'import from' statement above!"
 
